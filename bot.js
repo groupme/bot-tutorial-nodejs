d73fde5910f3747f31f7cdb38997dd7e58203317
batorobe <batorobe@msu.edu>
2015-01-22 15:04:43 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,16 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/, botFuckYou = /^\/fuck you$/;
-      
+      botRegex = /^\/cool guy$/;
+
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
+    this.res.end();
+  } 
+  else if(request.text && botRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postMessageF();
     this.res.end();
   } 
   else {
@@ -23,22 +28,6 @@ function postMessage() {
   var botResponse, options, body, botReq;
 
   botResponse = cool();
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-  
-  function postMessageF() {
-  var botResponse, options, body, botReq;
-
-  botResponse = "Fuck you too";
 
   options = {
     hostname: 'api.groupme.com',
@@ -69,6 +58,39 @@ function postMessage() {
   });
   botReq.end(JSON.stringify(body));
 }
+function postMessageF() {
+  var botResponse, options, body, botReq;
 
+  botResponse = "Fuck you too fag";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 
 exports.respond = respond;
