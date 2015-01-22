42ecb132bfff4d4fd06c8e2e0f820a9f38f4254c
batorobe <batorobe@msu.edu>
2015-01-22 15:42:32 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy/; botRegexF =  /^\/fuck you/; botRegexDL = /^\/DDL$/i
+      botRegex = /^\/cool guy/; botRegexF =  /^\/fuck you/; botRegexDL = /^\/DDL/i
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
