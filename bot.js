8270227636c050f68b9710678f22af7b3e6dc65e
batorobe <batorobe@msu.edu>
2015-01-25 10:32:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,8 +54,8 @@ function respond() {
   else if(request.text && botRegexP.test(request.text)) {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
-    req.replace(" ","+");
-    postMessage("http://daddyleagues.com/rMCF/team/"+req+"/depthchart");
+    req.replace(/ /,"+");
+    postMessage("http://daddyleagues.com/rMCF/players?name="+req+"&position=all&team=all");
     this.res.end();
   } 
   else {
