f8ee234a61c3d4a1b729de539d111c8ffa3c3967
batorobe <batorobe@msu.edu>
2015-01-25 11:40:35 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexYub = /^\/yubnub/
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexFG = /^\/fag/; botRegexSC = /^\/SDL/i
-      botRegexP = /^\/PDL/i; botRegexN = /nigger/i
+      botRegexP = /^\/PDL/i; botRegexN = /nigger/i; botRegexTw = /^\/twitch/i
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
@@ -58,9 +58,15 @@ function respond() {
     postMessage("http://daddyleagues.com/rMCF/players?name="+rep+"&position=all&team=all");
     this.res.end();
   } 
+  
   else if(request.text && botRegexN.test(request.text)) {
     this.res.writeHead(200);
     postMessage("HARD R ALERT");
+    this.res.end();
+  } 
+  else if(request.text && botRegexTw.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.twitch.tv/"+request.text.substring(8,request.text.length));
     this.res.end();
   } 
   else {
