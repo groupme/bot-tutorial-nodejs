b9fd61f5a428612722463c4977a49438b5caad8c
shoarora <shoarora@stanford.edu>
2016-04-14 17:02:24 -0700

    timer value changed

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ getSchedules();
 
 
 timeplan.repeat({
-  period: "10s",
+  period: "60s",
   task: function() {
     console.log('checking schedule, length is ' + curSchedule.length);
     for (var i = 0; i < curSchedule.length; i++) {
