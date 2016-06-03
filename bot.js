3a6f8229dbfdfe000c6cf72476ffa3a87c7442c3
Cody Wahl <codywahl@gmail.com>
2016-06-03 15:35:50 -0700

    update regex

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/next raid?$/;
+      botRegex = /^\/next raid when$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
