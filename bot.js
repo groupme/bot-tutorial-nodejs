026ac63081f1e01e2c041a14a0de5b61d9e6ceb0
StanleyTack <stanley.tack@users.noreply.github.com>
2016-07-11 20:53:33 -0700

    test message test update

--- bot.js
+++ bot.js
@@ -21,7 +21,8 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "Heroes never die";
+  //botResponse = cool();
 
   options = {
     hostname: 'api.groupme.com',
