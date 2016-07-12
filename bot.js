9fab6f1c54b6571432d890fca443e96e30237d6c
StanleyTack <stanley.tack@users.noreply.github.com>
2016-07-11 21:03:43 -0700

    change call sign

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+      botRegex = /^\/wifi$/;
+      //botRegex = /^\/cool guy$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
