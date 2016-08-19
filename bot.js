e7f5cde04a407b9afa716e743188924751dcd290
Alexdisch <alex.disch@gmail.com>
2016-08-19 16:58:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
+      botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexHarper = /^\/Harper/;botRegexSquirt = /^\/squirt/;botRegexBurn = /^\/burn/;botRegexTom = /^\/tom/;botRegexjj = /^\/jj/;botRegexRules = /^\/rules/;
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
       botRegexSiege = /^\/siege/
@@ -24,9 +24,29 @@ function respond() {
     postMessage("http://daddyleagues.com/NLB/team/"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
-  else if(request.text && botRegexSalt.test(request.text)) {
+  else if(request.text && botRegexHarper.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://i.imgur.com/B5BSVqH.png");
+    postMessage("http://36.media.tumblr.com/06086ac9d889c7729d928d4aa5d1405b/tumblr_nk8g0fFErk1s2yr14o1_1280.jpg");
+    this.res.end();
+  } 
+    else if(request.text && botRegexSquirt.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://media.giphy.com/media/3oEduY8Kh1AoOWUBnW/giphy.gif");
+    this.res.end();
+  } 
+  else if(request.text && botRegexBurn.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://45.media.tumblr.com/tumblr_ly1efoFthg1r4ghkoo1_500.gif");
+    this.res.end();
+  } 
+  else if(request.text && botRegexTom.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://media.giphy.com/media/Nccs0rNvVC1nG/giphy.gif");
+    this.res.end();
+  } 
+  else if(request.text && botRegexjj.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://media.giphy.com/media/ZQnr4SydezPLG/giphy.gif");
     this.res.end();
   } 
   else if(request.text && botRegexAd.test(request.text)) {
@@ -36,7 +56,7 @@ function respond() {
   }
   else if(request.text && botRegexRules.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://docs.google.com/document/d/1hSuEG7oplnx4IX6HGsMOjsWb9TCqC4-F1NLjuBz5PCM/edit");
+    postMessage("http://daddyleagues.com/NLB/rules");
     this.res.end();
   } 
   else if(request.text && botRegexGTA.test(request.text)) {
