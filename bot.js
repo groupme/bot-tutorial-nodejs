1eae3f2d6b8d62c6bae80b1c88074c36d863df8e
Alexdisch <alex.disch@gmail.com>
2016-10-13 21:23:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexHarper = /^\/Harper/;botRegexWatt = /^\/watt/;
+      botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexHarper = /^\/Harper/;botRegexFeeney = /^\/feeney/;
 botRegexReid = /^\/reid;botRegexBurn = /^\/burn/;botRegexTom = /^\/tom/;botRegexjj = /^\/jj/;botRegexRules = /^\/rules/;
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
@@ -30,9 +30,10 @@ botRegexReid = /^\/reid;botRegexBurn = /^\/burn/;botRegexTom = /^\/tom/;botRegex
     postMessage("http://36.media.tumblr.com/06086ac9d889c7729d928d4aa5d1405b/tumblr_nk8g0fFErk1s2yr14o1_1280.jpg");
     this.res.end();
   } 
-    else if(request.text && botRegexWatt.test(request.text)) {
+    else if(request.text && botRegexFeeney.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://media.giphy.com/media/3oEduY8Kh1AoOWUBnW/giphy.gif");
+
+postMessage("https://media1.giphy.com/media/RdsvvZXIzBgNq/giphy.gif");
     this.res.end();
   }  
     else if(request.text && botRegexReid.test(request.text)) {
