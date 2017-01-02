1bbf8442c1ef67298931d8c8d2fcce2dda572b77
Andrew Rothwell <a_rothwell@outlook.com>
2017-01-02 06:36:47 -0600

    Commit

---
+++ timebot.js
@@ -0,0 +1,39 @@
+var HTTPS = require('https');
+var cool = require('cool-ascii-faces');
+var botID = process.env.BOT_ID;
+
+PostMessage("PollBotPlus's timer function has been started");
+
+function called(){
+	this.res.writeHead(200);
+	PostMessage("The repeted task has been completed");
+	this.res.end();
+}
+function PostMessage(botResponse) {
+  var botResponse, options, body, botReq;
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+  console.log('sending ' + botResponse + ' to ' + botID);
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+exports.respond = respond;
