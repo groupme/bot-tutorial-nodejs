701616545661c0804610c0de4a5c9c9b5f4069f1
Andrew Rothwell <a_rothwell@outlook.com>
2017-01-02 03:30:17 -0600

    Create unknown command function

--- bot.js
+++ bot.js
@@ -10,24 +10,23 @@ function respond() {
 
   if(request.text && face.test(request.text)) {
     this.res.writeHead(200);
-    faceMessage();
+    postMessage(cool());
     this.res.end();
   } 
   else if(request.text && time.test(request.text)){
     this.res.writeHead(200);
-    timeMessage();
+    postMessage("New Time");
     this.res.end();
   }
   else {
-    console.log("don't care");
+   	postMessage("Unknown Command");
     this.res.writeHead(200);
     this.res.end();
   }
 }
 
-function faceMessage() {
+function PostMessage(botResponse) {
   var botResponse, options, body, botReq;
-  botResponse = cool();
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
@@ -53,34 +52,4 @@ function faceMessage() {
   });
   botReq.end(JSON.stringify(body));
 }
-
-function timeMessage() {
-  var botResponse, options, body, botReq;
-  botResponse = new Date.now();
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-  console.log('sending ' + botResponse + ' to ' + botID);
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
-
 exports.respond = respond;
