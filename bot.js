b665975df01df22367538cbe77a96d9ae5d6facb
Andrew Rothwell <a_rothwell@outlook.com>
2017-01-02 04:57:00 -0600

    Update

--- bot.js
+++ bot.js
@@ -5,10 +5,10 @@ var botID = process.env.BOT_ID;
 PostMessage("PollBotPlus Restarted" + "\n" + "for list of possible commands /help");
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]);
-  var face = /^\/face$/;
-  var time = /^\/time$/;
-	var help = /^\/help$/
+	var request = JSON.parse(this.req.chunks[0]);
+	var face = /^\/face$/;
+	var time = /^\/time$/;
+	var help = /^\/help$/;
 
   if(request.text && face.test(request.text)) {
     this.res.writeHead(200);
