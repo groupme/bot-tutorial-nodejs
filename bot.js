2c6af3881b84f1f0941bc70773786126806db02f
Andrew Rothwell <a_rothwell@outlook.com>
2017-01-02 02:44:44 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,63 +4,21 @@ var cool = require('cool-ascii-faces');
 var botID = process.env.BOT_ID;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]);
-  var newFace = /^\/face$/;
-  var tellTime = /^\/time$/;
+  var request = JSON.parse(this.req.chunks[0]),
+      botRegex = /^\/new commandy$/;
 
-  if(request.text && newFace.test(request.text)){
+  if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    newFace();
+    postMessage();
     this.res.end();
-  } 
-  else if(request.text && tellTime.test(request.text)){
-    this.res.writeHead(200);
-    tellTime();
-    this.res.end();
-  }
-  else {
+  } else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
 }
 
-function tellTime(){
-    var botResponse, options, body, botReq;
-
-  botResponse = this.getDate();
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
-
-function newFace() {
+function postMessage() {
   var botResponse, options, body, botReq;
 
   botResponse = cool();
