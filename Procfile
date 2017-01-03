362c14b2ebc8cc50d3e7d7d63465e00f0820d36f
Andrew Rothwell <a_rothwell@outlook.com>
2017-01-02 19:48:21 -0600

    Commit

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: node index.js
 timer: node worker.js
+talker: node speak.js
---
+++ quotebot.js
@@ -0,0 +1,39 @@
+var HTTPS = require('https');
+var botID = process.env.BOT_ID;
+var rqg = require('random-qoutes-generator');
+
+PostMessage(rqg());
+
+function respond() {
+	var request = JSON.parse(this.req.chunks[0]);
+	this.res.writeHead(200);
+	PostMessage(rqg());
+	this.res.end();
+}
+
+
+function PostMessage(botResponse) {
+  var botResponse, options, body, botReq;
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+  console.log('sending ' + botResponse + ' to ' + botID);
+  botReq = HTTPS.request(options, function(res) {
+	console.log('Line 19');
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+	  console.log('Line 25');
+  });
+  console.log('Line 27');
+  botReq.end(JSON.stringify(body));
+}
+exports.respond = respond;
