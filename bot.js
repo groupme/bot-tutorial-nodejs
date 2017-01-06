2a8d7943f9b7520f0a329989b4eef4995c44de5d
Andrew Rothwell <a_rothwell@outlook.com>
2017-01-05 19:46:56 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ function respond() {
 	var cat = /cat/gi;
 	var comm = /(?:commie|communism|communist)/gi;
 	var dead = /(?:dead|death|dying|die)/gi;
-
+console.log(request);
   if(request.text && face.test(request.text)) {
     this.res.writeHead(200);
     PostMessage(cool());
