5740898d92e387111a686aeae9f7156cabad0cfa
julius agno <agno.julius@gmail.com>
2017-07-17 18:37:51 -0500

    trying switch case

--- bot.js
+++ bot.js
@@ -41,6 +41,7 @@ function postMessage(request) {
 		botResponse = "yes";
    
 	}else if (/\!doggopls/i.test(request)){
+		/*
 		var x = Math.floor((Math.random() * 3) + 1);
 		if (x == 1) {
 			botResponse = "yes";
@@ -48,7 +49,12 @@ function postMessage(request) {
 			botResponse = "no";
 		} else if (x == 3) {
 			botResponse = "maybe";
-		}
+		}*/
+		switch(Math.floor((Math.random() * 3) + 1);
+		case 0: botResponse = "yes"; break;
+		case 1: botResponse = "no"; break;
+		case 2: botResponse = "maybe"; break;
+		
 	}
 
   options = {
