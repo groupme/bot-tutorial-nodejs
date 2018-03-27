2f1a23ba68cf0115b97bc7705eba5c1f3efca1bd
Lucas Chen <qchen123@yahoo.com>
2018-03-26 23:28:57 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,8 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       winRegex = /^\E we won$/,
       lostRegex = /^\E we lost$/,
-      helloRegex = /^\Hey E$/;
+      helloRegex = /^\Hey E$/,
+      dogHouseRegex = /^\/Doghouse$/;
 
   if(request.text && lostRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -20,6 +21,10 @@ function respond() {
   }else if(request.text && helloRegex.test(request.text)) {
     this.res.writeHead(200);
     postHelloMessage();
+    this.res.end();
+  }else if(request.text && dogHouseRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postDoghouseMessage();
     this.res.end();
   }else {
     console.log("don't care");
@@ -133,5 +138,39 @@ function postLostMessage() {
   botReq.end(JSON.stringify(body));
 }
 
+function postDoghouseMessage() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "I can really tell that Lucas really loves Brooke. What's even more important is that Lucas definitely didn't program me to say this in case there's a day he's in the doghouse.";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 
 exports.respond = respond;
