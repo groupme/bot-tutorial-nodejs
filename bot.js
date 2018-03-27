b9f0c56ac6b48c2b7e39d17bea255ad4ba8727d2
Lucas Chen <qchen123@yahoo.com>
2018-03-26 23:45:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
+var quotes = require('awesome-quotes');
 
 var botID = process.env.BOT_ID;
 
@@ -8,7 +9,8 @@ function respond() {
       winRegex = /^\E we won$/,
       lostRegex = /^\E we lost$/,
       helloRegex = /^\Hey E$/,
-      dogHouseRegex = /^\/Doghouse$/;
+      dogHouseRegex = /^\/Doghouse$/,
+      quoteRegex = /^\/quote/;
 
   if(request.text && lostRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -26,11 +28,58 @@ function respond() {
     this.res.writeHead(200);
     postDoghouseMessage();
     this.res.end();
+  }else if(request.text && quoteRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postQuoteMessage();
+    this.res.end();
   }else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
+}
+
+
+
+
+function postQuoteMessage() {
+  var botResponse, options, body, botReq;
+
+  quotes.getQuote('football','en', function(err, data) {
+    if(!err) {
+        console.log(data);
+        botResponse = data;
+    }
+  });
+  
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
 }
 
 function postHelloMessage() {
