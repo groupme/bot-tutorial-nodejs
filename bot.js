753ac32724e416c42c5a13a351d20e47a08c6a8a
Lucas Chen <qchen123@yahoo.com>
2018-03-26 22:59:24 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/test guy$/;
+      botRegex = /^\/Eric$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -19,6 +19,41 @@ function respond() {
 }
 
 function postMessage() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "hello";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postMessage2() {
   var botResponse, options, body, botReq;
 
   botResponse = cool();
