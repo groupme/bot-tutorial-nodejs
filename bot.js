95e03b2def790de22b4f8a77522b86f149cf4991
Lucas Chen <qchen123@yahoo.com>
2018-03-26 23:07:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,13 +5,19 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/Eric$/;
+      botRegex = /^\/Eric$/,
+      secondRegex = /^\Cool$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
+    postMessage2();
+    this.res.end();
+  } elseif(request.text && secondRegex.test(request.text)){
+    this.res.writeHead(200);
     postMessage();
     this.res.end();
-  } else {
+  }
+  else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
