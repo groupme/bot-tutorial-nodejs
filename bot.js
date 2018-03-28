f2aac0050830a9ac7c0c6f1ab0a97991c8a1eaf0
Lucas Chen <qchen123@yahoo.com>
2018-03-27 22:29:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 var starwars = require("starwars")
+var oneLinerJoke = require("one-liner-joke");
 
 var botID = process.env.BOT_ID;
 
@@ -10,7 +11,8 @@ function respond() {
       lostRegex = /^\E we lost$/,
       helloRegex = /^\Hey E$/,
       dogHouseRegex = /^\/Doghouse$/,
-      quoteRegex = /^\/quote/;
+      quoteRegex = /^\/quote/,
+      jokeRegex = /^\/joke/;
 
   if(request.text && lostRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -32,6 +34,10 @@ function respond() {
     this.res.writeHead(200);
     postQuoteMessage();
     this.res.end();
+  }else if(request.text && jokeRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postJokeMessage();
+    this.res.end();
   }else {
     console.log("don't care");
     this.res.writeHead(200);
@@ -40,7 +46,40 @@ function respond() {
 }
 
 
+function postJokeMessage() {
+  var botResponse, options, body, botReq;
 
+  botResponse = oneLinerJoke.getRandomJoke();
+  
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 
 function postQuoteMessage() {
   var botResponse, options, body, botReq;
