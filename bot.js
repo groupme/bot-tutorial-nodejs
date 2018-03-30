1f8e29857201d9e51b6e200727fcaf5713b9953d
Lucas Chen <qchen123@yahoo.com>
2018-03-29 22:39:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 var starwars = require("starwars")
 var oneLinerJoke = require("one-liner-joke");
+var chuckNorrisJokes = require("chuck-norris-jokes")
 
 var botID = process.env.BOT_ID;
 
@@ -12,7 +13,8 @@ function respond() {
       helloRegex = /^\Hey E$/,
       dogHouseRegex = /^\/Doghouse$/,
       quoteRegex = /^\/quote/,
-      jokeRegex = /^\/joke/;
+      jokeRegex = /^\/joke/,
+      chuckRegex = /^\/chuck/;
 
   if(request.text && lostRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -38,6 +40,10 @@ function respond() {
     this.res.writeHead(200);
     postJokeMessage();
     this.res.end();
+  }else if(request.text && chuckRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postChuckMessage();
+    this.res.end();
   }else {
     console.log("don't care");
     this.res.writeHead(200);
@@ -45,6 +51,46 @@ function respond() {
   }
 }
 
+function postChuckMessage() {
+  var botResponse, options, body, botReq, jokePromise;
+  
+  jokePromise = chuckNorrisJokes.hitme();
+  jokePromise.then(function(value){
+    botResponse = value;
+    options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+});
+
+  //botResponse =  oneLinerJoke.getRandomJokeWithTag('dirty', 'stupid', 'insults', 'blonde').body;
+  
+  
+}
 
 function postJokeMessage() {
   var botResponse, options, body, botReq;
