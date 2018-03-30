69e2c320651dd13d5c5762ec1e86cca8d188fec0
Lucas Chen <qchen123@yahoo.com>
2018-03-29 23:01:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,8 @@ function respond() {
       helloRegex = /^\Hey E$/,
       dogHouseRegex = /^\/Doghouse$/,
       quoteRegex = /^\/quote/,
-      jokeRegex = /^\/joke/;
+      jokeRegex = /^\/joke/,
+      chuckRegex = /^\/chuck/;
 
   if(request.text && lostRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -39,11 +40,50 @@ function respond() {
     this.res.writeHead(200);
     postJokeMessage();
     this.res.end();
+  }else if(request.text && chuckRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postChuckMessage();
+    this.res.end();
   }else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
+}
+
+function postChuckMessage() {
+  var botResponse, options, body, botReq;
+
+  botResponse =  oneLinerJoke.getRandomJokeWithTag('dirty', 'stupid', 'insults', 'blonde').body;
+  
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
 }
 
 function postJokeMessage() {
