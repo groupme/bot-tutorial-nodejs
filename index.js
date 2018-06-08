d9bc3ebc4756b82ee6c5ddb672b7c0aeb93c9964
Justin Behanna <j.behanna@gmail.com>
2018-06-08 18:01:19 -0400

    test deploy

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ server.listen(port);
 
 function ping() {
   this.res.writeHead(200);
-  this.res.end('Hey, check me out: <a href="http://nvn.io">nvn.io</a>');
+  this.res.end('Hey, check me out: <a href="http://nvn.io">nvn.io</a>!');
 }
