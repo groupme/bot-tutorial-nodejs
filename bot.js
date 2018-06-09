50cd77fac2604edef3f03f796eb63cdf4b587dde
Justin Behanna <j.behanna@gmail.com>
2018-06-09 01:11:21 -0400

    do it

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^hype[1-3]/im;
+      botRegex = /^!hype[1-3]/im;
 
   if (request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
