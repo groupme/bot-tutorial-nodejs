9f890c603bbab5d3f2ac2f8e9c231e2cd9582de5
Justin Behanna <j.behanna@gmail.com>
2018-06-10 15:34:03 -0400

    var fix

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      hypeMode = /^!hypemode/im;
+      hypeMode = /^!hypemode/im,
       hypeCalls = /^!hype([1-3])/im;
 
   if (request.text && hypeCalls.test(request.text)) {
