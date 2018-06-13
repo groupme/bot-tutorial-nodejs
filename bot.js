baab5c60a2b1a6f16f25936b6644128b65b56e50
faizanv <faizan.virani.44@gmail.com>
2018-06-12 22:42:22 -0400

    Mosaic time

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = 'Mosaic Time! ' + cool();
 
   options = {
     hostname: 'api.groupme.com',
