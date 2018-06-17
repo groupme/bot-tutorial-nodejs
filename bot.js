be942e84cdb1c30192419dfdb2505ac17bbfe735
Justin Behanna <j.behanna@gmail.com>
2018-06-17 14:41:43 -0400

    qupte typo

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ var quotes = [
   '"The translation software I have here is a bit rusty, but I believe they\'re saying some extremely rude things about your mother." - Devrim Kay',
   '"I used a pretty nasty word to describe Ghaul. I don\'t want to repeat it here." - Ghost',
   '"Does that fallen look like a transmat thieving thief to you? He looks like a transmat theieving theif to me." - Ghost',
-  '"That one. That one\'s gotta me a transmat thieving thief. He looks even thiefier than the first one." - Ghost',
+  '"That one. That one\'s gotta be a transmat thieving thief. He looks even thiefier than the first one." - Ghost',
   '"While I cannot condone theft, I do appreciate payback." - Zavala',
   '"Only my captain can access those memories. And he\'s super dead." - Failsafe',
   '"Got em! We got their spire. You should have seen us, we were amazing!" - Ghost',
