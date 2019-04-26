628e7d1a55d0845751d37159e5d92efd4fae0622
Aaron Lewkowiez <al06208@georgiasouthern.edu>
2019-04-25 20:48:28 -0400

    Patchnotes, penguins, and 8balls
    woohoo

--- bot.js
+++ bot.js
@@ -7,10 +7,11 @@ var img2 = "https://img.fireden.net/v/image/1540/77/1540777065833.png";
 var img3 = "https://i.redd.it/s3p6op2feg311.jpg";
 var img0 = "https://i.kym-cdn.com/photos/images/original/001/290/315/b55.jpeg";
 function respond() {
-    var request = JSON.parse(this.req.chunks[0]), botRegex1 = /^\/cool guy$/, botRegex2 = /.*[Nn].[Gg][Gg].[Rr].*/;
+    var request = JSON.parse(this.req.chunks[0]), botRegex1 = /^\/cool guy$/, botRegex2 = /.*[Nn].[Gg][Gg].[Rr].*/, botRegex3 = /^\/8ball$/, botRegex4 = /^\/patchnotes$/;
 
     var reg1 = botRegex1.test(request.text);
     var reg2 = botRegex2.test(request.text);
+    var reg3 = botRegex3.test(request.text);
 
     if (request.text && reg1) {
         this.res.writeHead(200);
@@ -31,11 +32,34 @@ function respond() {
                 postMessage(img2);
                 break;
             default:
-                postMessage(img3);;
+                postMessage(img3);
                 break;
         }
         postMessage('YOU CAN\'T SAY THAT THAT\'S RACIST');
-        this.res.end;
+        this.res.end();
+    }
+    else if (request.text && reg3) {
+        this.res.writeHead(200);
+        switch (Math.floor(Math.random() * 3)) {
+            case 0:
+                postMessage('Yes, friend');
+                break;
+            case 1:
+                postMessage('I\'m sorry, no');
+                break;
+            case 2:
+                postMessage('Ask again later');
+                break;
+            default:
+                postMessage('oh god oh fuck');
+                break;
+        }
+        this.res.end();
+    }
+    else if (request.text && reg4) {
+        this.res.writeHead(200);
+        postMessage('Beemisbot v1.3 Patch notes:');
+        postMessage('-Penguin memes added to bad word detection\n-New command: /8ball . Ask a question, then get an answer.\n-Added /patchnotes for Cameron. Fuck you')
     }
     else {
     console.log("don't care");
