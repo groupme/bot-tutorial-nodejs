29ca4faae53976a300c93221f2bd8aa5e4936575
cpace8701 <50055714+cpace8701@users.noreply.github.com>
2019-04-27 14:54:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,7 +63,7 @@ function respond() {
         postMessage('Beemisbot v1.4 Patch Notes: The Penguin Patch 2.0\n-Penguin Image Processing is improved, extra penguins added, many bugs fixed, all is good');
         this.res.end();
     }
-    else if (request.text && reg2) {
+    else if (request.text && reg7) {
         this.res.writeHead(200);
         switch (Math.floor(Math.random()*4)){
            case 0:
