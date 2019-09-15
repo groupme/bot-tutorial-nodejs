5ce2ed5aa2afb3d2fbd977cf044d6d69d027d431
ErikBoesen <me@erikboesen.com>
2019-09-15 01:11:02 -0400

    Clean up some JavaScript code

--- bot.js
+++ bot.js
@@ -7,12 +7,12 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/;
 
-  if(request.text && botRegex.test(request.text)) {
+  if (request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
   } else {
-    console.log("don't care");
+    console.log('Received irrelevant message.');
     this.res.writeHead(200);
     this.res.end();
   }
@@ -30,18 +30,18 @@ function postMessage() {
   };
 
   body = {
-    "bot_id" : botID,
-    "text" : botResponse
+    'bot_id' : botID,
+    'text' : botResponse
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
 
   botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
+    if(res.statusCode == 202) {
+      // Request was successful.
+    } else {
+      console.log('received bad status code ' + res.statusCode);
+    }
   });
 
   botReq.on('error', function(err) {
